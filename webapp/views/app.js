3f9659b3b7bdc7aa2b4de5b4a557a3abf73b14e0
Edith-Mounting <rajbansal.hello@gmail.com>
2021-12-24 09:38:54 +0530

    backend intigrated

---
+++ webapp/views/app.js
@@ -0,0 +1,6 @@
+const executeBtn = document.querySelector('#id_btn');
+const scripts_options = document.querySelector('#id_select');
+// console.log("changeddddddddddd");
+
+scripts_options.addEventListener('click', () => {
+});
